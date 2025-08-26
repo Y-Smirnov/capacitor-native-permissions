e9b1b4b131fd2a0e3df6db46247571ccb4e8d717
yevheniismirnov <yevhenii.smirnov@outlook.com>
2025-08-26 22:47:58 +0100

    chore: add missing models export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './native-permissions';
 export * from './models/permission-status';
 export * from './models/permission-notifications';
+export * from './models/bluetooth-android-permissions';
+export * from './models/calendar-permissions';
+export * from './models/permission-status';
+export * from './models/supported-permissions';
