776d50e61ee7b6c00784def1c0da65682e927a3e
yevheniismirnov <yevhenii.smirnov@outlook.com>
2026-05-17 21:11:57 +0200

    chore: keep platform requirements unchanged

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorNativePermissions",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "CapacitorNativePermissions",
